36dda22f60f3ee715fa589008ed26662124fd9b5
luan.silva <luan.silva@virtus.ufcg.edu.br>
2022-04-24 19:50:20 -0300

    Create and list users

--- develcode-back/src/index.js
+++ develcode-back/src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const userService = require('./services/user.service');
 
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
